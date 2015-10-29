2d41954e9d427eda58a539b2bd14016288022799
Subhash Sharma <subhash@modelogiq.com>
2015-10-29 06:47:38 +0530

    facebook oauth working now, need to implement it as promise based calls

--- src/oauth-logic.js
+++ src/oauth-logic.js
@@ -1,6 +1,8 @@
+var https = require("https");
 var express = require("express");
 var bodyParser = require("body-parser");
 var passport = require("passport");
+var url = require("url");
 var FacebookStrategy = require("passport-facebook").Strategy;
 var AppId = "1658722211069748";
 var AppVersion = "2.5";
@@ -11,41 +13,48 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded());
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function(user, done){
-	done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-	done(null, obj);
-});
-
-passport.use(new FacebookStrategy({
-	clientID: AppId,
-	clientSecret: AppSecret,
-	callbackURL: "http://127.0.0.1:4000/auth/facebook/callback",
-	proof: false
-}, function(accessToken, refreshToken, profile, done) {
-	return done(null, profile);
-}));
 
 app.route("/").get(function(req, res){
 	res.write("welcome");
 	res.send();
 });
 
-app.get("/auth/facebook", passport.authenticate("facebook"));
-// function(req, res) {
-// 	console.log(req.body);
-// 	res.send();
-// });
-
-app.get("/auth/facebook/callback", passport.authenticate("facebook"), function(req, res) {
-	console.log(req);
+app.get("/auth/facebook", function(req, res) {
+	res.writeHead(302, {
+		Location: "https://www.facebook.com/dialog/oauth?client_id="+AppId+"&redirect_uri=http://localhost:4000/auth/facebook/callback&response_type=code"
+	});
 	res.send();
 });
 
+var calledOnce = false;
+var tokenObject = {};
+app.get("/auth/facebook/callback", function(req, res) {
+	var code = url.parse(req.url).query.split("=")[1];
+	getAccessToken(code, function(str){
+		// write the access_token to a db entry and then redirect
+		tokenObject = JSON.parse(str);
+		res.writeHead(302, {
+			Location: "http://localhost:4000/auth/facebook/done"
+		});
+		res.send();
+	});
+});
+
+app.get("/auth/facebook/done", function(req, res) {
+	res.send(tokenObject);
+});
+
+function getAccessToken(code, callback) {
+	var requestOptions = {
+		host: "graph.facebook.com",
+		path: "/v2.3/oauth/access_token?client_id="+AppId+"&client_secret="+AppSecret+"&code="+code+"&redirect_uri=http://localhost:4000/auth/facebook/callback"
+	};
+	var callbackHandler = function(res) {
+		var str = "";
+		res.on("data", function(d){str = str + d.toString()});
+		res.on("end", function(d){callback(str)});
+	};
+	https.request(requestOptions, callbackHandler).end();
+}
 
 app.listen(4000, "localhost");
